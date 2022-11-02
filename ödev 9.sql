b3899b64a201af90051c7895b88241423f3dec22
Ali ERKAN <92388359+alierkan704@users.noreply.github.com>
2022-11-02 11:03:20 +0300

    Add files via upload

---
+++ ödev 9.sql
@@ -0,0 +1,6 @@
+--SELECT city,country FROM country
+--JOIN city ON city.country_id = country.country_id;
+--SELECT payment_id,first_name,last_name FROM customer
+--JOIN payment ON customer.customer_id = payment.customer_id;
+SELECT rental_id,first_name,last_name FROM customer
+JOIN rental ON customer.customer_id = rental.customer_id;
